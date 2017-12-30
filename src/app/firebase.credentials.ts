ab32098feb84ee3ed9ab22f82fc8f542c91c930f
Timezer <perrault.william2@gmail.com>
2017-12-30 22:25:06 +0100

    Premier commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAivLp3Bp9uE6OTWeKuJHpSiq4MavzxI34",
+    authDomain: "williamperrault-mobile-4a.firebaseapp.com",
+    databaseURL: "https://williamperrault-mobile-4a.firebaseio.com",
+    projectId: "williamperrault-mobile-4a",
+    storageBucket: "williamperrault-mobile-4a.appspot.com",
+    messagingSenderId: "488627303217"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../../models/item/item.model';
+import { ToastService } from '../../services/toast/toast.service';
 
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    shoppingList$: Observable<Item[]>;
 
-  }
+    constructor(public navCtrl: NavController, private shopping: ShoppingListService, private toast: ToastService) {
+        this.shoppingList$ = this.shopping
+            .getShoppingList() //DB LIST
+            .snapshotChanges() //Key and Value
+            .map(
+            changes => {
+                return changes.map(c => ({
+                    key: c.payload.key,
+                    ...c.payload.val()
+                }));
+            }
+            );
+    }
 
+    showHelp() {
+        this.toast.show(`Veuillez vous connectez à internet pour récupérer les données de la base de données!`);
+    }
 }
